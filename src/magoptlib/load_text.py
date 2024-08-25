6b4a9944fc525e3aae6fa1764c6c4696e31d63d4
Palma Inczeffy <inczeffy@gmail.com>
2024-08-25 21:12:48 +0200

    fix: add error handling

--- src/magoptlib/load_text.py
+++ src/magoptlib/load_text.py
@@ -1,34 +1,16 @@
 import json
 
 
-def load_text(input_file):
-    """ Load text from the input file.
+def text_to_array(input_file):
+    """ Convert the input file to arrays.
+
+    Fills up b_values, phi_values, and theta_values from a json file.
 
     Parameters
     ----------
+
     input_file : str
         The input file in json format.
-
-    Returns
-    -------
-    data : dict
-        The data from the input file, in a dictionary format.
-
-    """
-    with open(input_file, "r") as f: 
-        data = json.load(f)
-    return data
-
-def text_to_array(data):
-    """ Convert the dictionary to arrays.
-
-    Fills up the b_values, phi_values, and theta_values arrays from the dictionary.
-
-    Parameters
-    ----------
-
-    data : dict
-        The data from the input file, in a dictionary format.
 
     Returns
     -------
@@ -36,20 +18,38 @@ def text_to_array(data):
         The spherical harmonics signal, in our case the magnetic field values.
 
     phi_values : list
-        The azimuthal angle values.  
+        The azimuthal angle values.
 
     theta_values : list
-        The polar angle values.  
-
+        The polar angle values.
     """
-    b_values =[]
+
+    if not isinstance(input_file, str):
+        raise TypeError("Input data should be a JSON string.")
+
+    try:
+        with open(input_file, "r") as f:
+            data = json.load(f)
+    except json.JSONDecodeError as e:
+        raise TypeError("Input data is not valid JSON.") from e
+
+    if not isinstance(data, dict):
+        raise TypeError("The JSON data should be a dictionary.")
+
+    b_values = []
     phi_values = []
     theta_values = []
 
-    n=len(data['data'])
+    n = len(data['data'])
     for i in range(n):
-        b_values.append(data['data'][i]['value'])
-        phi_values.append(data['data'][i]['phi'])
-        theta_values.append(data['data'][i]['theta'])
+
+        entry = data['data'][i]
+
+        if 'value' not in entry or 'phi' not in entry or 'theta' not in entry:
+            raise KeyError("Dictionary must contain 'value', 'phi', and 'theta' keys.")
+
+        b_values.append(entry['value'])
+        phi_values.append(entry['phi'])
+        theta_values.append(entry['theta'])
 
     return b_values, phi_values, theta_values
